0b7abc1294f089fe8933359b378d6f302bf3765e
vishalshingate <vishalshingate26@gmail.com>
2025-12-02 08:26:45 +0530

    1.Added the Integration test folder

---
+++ src/test/java/integration/com/learnkafka/controller/LibraryEventsControllerIntegrationTest.java
@@ -0,0 +1,35 @@
+package com.learnkafka.controller;
+
+import com.learnkafka.dto.LibraryEvent;
+import com.learnkafka.util.TestUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class LibraryEventsControllerIntegrationTest {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    void postLibraryEvent() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("content-type", MediaType.APPLICATION_JSON.toString());
+
+        var httpEntity = new HttpEntity<>(TestUtil.libraryEventRecord(), httpHeaders);
+
+        var responseEntity =
+        restTemplate.exchange("/v1/libraryevent", HttpMethod.POST,httpEntity , LibraryEvent.class);
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+
+    }
+}
