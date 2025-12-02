92e087bbbfccf6538da44a7175afab1dd72778b9
vishalshingate <vishalshingate26@gmail.com>
2025-12-02 09:18:11 +0530

    Added the test case for consume the message from kafka

--- src/test/java/integration/com/learnkafka/controller/LibraryEventsControllerIntegrationTest.java
+++ src/test/java/integration/com/learnkafka/controller/LibraryEventsControllerIntegrationTest.java
@@ -1,7 +1,16 @@
 package com.learnkafka.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.learnkafka.dto.LibraryEvent;
 import com.learnkafka.util.TestUtil;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,19 +20,58 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.TestPropertySource;
 
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
+/** test Scenario
+ * 1. Configure embedded Kafka broker for testing
+ * 2 .overrise the kafka producer bootstrap server property to point to embedded kafka broker
+ * 3. Configure the kafka consumer in the test to listen to the topic
+ * 4 .Wire kafka consumer and Embedded kafka broker
+ * 5 .Consume the record from thr Embedded kafka broker and then assert the results
+ */
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@EmbeddedKafka(topics = "library-events", partitions = 3)
+@EmbeddedKafka(topics = "library-events")
 @TestPropertySource(properties = {"spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
     "spring.kafka.admin.bootstrap-servers=${spring.embedded.kafka.brokers}"})
 class LibraryEventsControllerIntegrationTest {
 
     @Autowired
     TestRestTemplate restTemplate;
+
+    @Autowired
+    EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+
+    private Consumer<Integer, String> consumer;
+    @BeforeEach
+    void setUp() {
+        //wire the consumer with embedded kafka broker
+        var configs = new HashMap<>(
+                KafkaTestUtils.consumerProps("group1", "true", embeddedKafkaBroker));
+        configs.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"latest");
+
+        consumer = new DefaultKafkaConsumerFactory<>(configs, new IntegerDeserializer(), new StringDeserializer())
+            .createConsumer();
+
+        // subscribe consumer to embedded kafka topics
+        embeddedKafkaBroker.consumeFromAllEmbeddedTopics(consumer);
+    }
+
+    @AfterEach
+    void tearDown() {
+        consumer.close();
+    }
 
     @Test
     void postLibraryEvent() {
@@ -36,5 +84,15 @@ class LibraryEventsControllerIntegrationTest {
         restTemplate.exchange("/v1/libraryevent", HttpMethod.POST,httpEntity , LibraryEvent.class);
         assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
 
+
+        // consume records from  embedded kafka broker
+
+        ConsumerRecords<Integer,String> consumerRecords = KafkaTestUtils.getRecords(consumer);
+
+        assert consumerRecords.count() == 1;
+        consumerRecords.forEach(record -> {
+            var libraryEventActual= TestUtil.parseLibraryEventRecord(objectMapper, record.value());
+            assertEquals(libraryEventActual, TestUtil.libraryEventRecord());
+        });
     }
 }
